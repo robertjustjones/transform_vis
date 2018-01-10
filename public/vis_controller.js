be04229f2ce65ecaed037c007e1019913f37af66
Robert Jones <robert@justjones.org>
2018-01-10 14:02:04 -0600

    Add G2 to plugin context.

--- public/vis_controller.js
+++ public/vis_controller.js
@@ -6,6 +6,9 @@ class VisController {
     this.container = document.createElement('div');
     this.container.className = 'output-vis';
     this.el.appendChild(this.container);
+    this.g2 = document.createElement('script');
+    this.g2.src = 'https://s3-us-west-2.amazonaws.com/misc.hop.market/g2.min.js';
+    this.el.appendChild(this.g2);
   }
 
   render(visData, status) {
